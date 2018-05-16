4f4583f2849b273bf66dacd69c92bd0acbc61303
Tbo8098 <thibault.tyler@gmail.com>
2018-05-16 08:35:50 +0700

    commiting files: run, student_info.json, launch.json

---
+++ Run.py
@@ -0,0 +1,121 @@
+import json
+import sys
+import os
+
+class user_class():
+    def __init__(self, name):
+        self.name = name
+        self.password = password
+
+    @classmethod
+    def create_new_user(clf):
+        newUserCreated = False
+        while newUserCreated is False:
+            name = input("name: ")
+            confermation = input(f"your name is: {name}. Is this correct?")
+
+            if confermation == 'Y' or confermation == 'y' or confermation == 'yes' or confermation == 'Yes' or confermation == 'YES':
+                password = input("password: ")
+                confermation = input("retype password: ")
+                if confermation == password:
+                    print("all done")
+                    newUserCreated = True
+                    user_class.add_to_user_list(name, password)
+                    user_class.create_user_file(name)
+                else:
+                    print("your passwords do not match")
+            else:
+                pass
+
+    def add_to_user_list(name, password):
+        load_file()
+        usersInFile = fileLoaded["users"]
+
+        usersInFile.update({
+            name:{
+                "userName": name,
+                "password": password
+            }
+        })
+
+        path = "C:\\Users\Spectre\Documents\Programming\Student Password manager\student_info.json"
+
+        with open(path, 'w') as outfile:
+            json.dump(fileLoaded, outfile)
+
+        outfile.close()
+    
+    def create_user_file(username):
+        fileName = username + ".json"
+        f = open(fileName, 'w')
+        data = {
+        }
+        json.dump(data, f)
+
+def load_file():
+    global fileLoaded
+
+    path = "C:\\Users\Spectre\Documents\Programming\Student Password manager\student_info.json"
+
+    fileSize = os.path.getsize(path)
+
+    if fileSize > 0:
+        f = open(path, 'r')
+        fileLoaded = json.load(f)
+        f.close()
+        return
+
+    else:
+        f = open(path, 'w')
+        data = {
+            "users": {
+
+            }
+        }
+        json.dump(data, f)
+        f.close()
+        return
+
+
+def login():
+    userName = input("user name: ")
+
+    if userName == "new user":
+        user_class.create_new_user()
+    
+    elif userName == "quit" or userName == 'q':
+        sys.exit(0)
+
+    else:
+        password = input("password: ")
+        listOfUsers = fileLoaded["users"].keys()
+        userInList = userName in listOfUsers
+
+        if userInList is True:
+            name = fileLoaded["users"][userName]["userName"]
+            if userName == name:
+                    passwrd = fileLoaded["users"][userName]["password"]
+                    if password == passwrd:
+                        print("you're in!")
+                    print("Username or password incorrect")
+        else:
+            print("Username or password incorrect")
+    
+    return True
+
+def mainMenu():
+    print("main menu")
+
+def run(fileIsLoaded):
+
+    while True:
+        if not fileIsLoaded:
+            load_file()
+            login()
+        
+        else:
+            mainMenu()
+
+
+
+run(False)
